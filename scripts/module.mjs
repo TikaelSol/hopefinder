2c03ae0b38112a2a7fe4ae518a52164bc4941746
TikaelSol <80183198+TikaelSol@users.noreply.github.com>
2022-12-18 20:37:17 -0500

    Version 0.9 - Initial Upload
    Contains all data entry from Survivor's Guide, initial journal pop-up, and automation.

---
+++ scripts/module.mjs
@@ -0,0 +1,7 @@
+export default {
+  moduleId: "hopefinder",
+  packName: "hopefinder",
+  journalFlag: "isHopefinder",
+  cssClass: "pf2e-hopefinder",
+  languages: ["en"]
+}
---
+++ scripts/register-journal-style.mjs
@@ -0,0 +1,31 @@
+import CONFIG from "./module.mjs";
+
+function registerSettings() {
+  game.settings.register("hopefinder", "firstTime", {
+    name: "Initial Pop-Up",
+    scope: "client",
+    config: false,
+    type: Boolean,
+    default: true
+})
+};
+
+Hooks.once("init", () => {
+  registerSettings();
+});
+
+Hooks.once('ready', async function () {
+    if (game.user.isGM) {
+        if (game.settings.get("hopefinder", "firstTime") == true) {
+            const journals = await game.packs.get("hopefinder.hopefinder-journals").getDocuments();
+            const journal = journals.filter(j => j.id == "7NSV1EbfvS7tOwls")[0]
+            journal.sheet.render(true)
+            game.settings.set("hopefinder", "firstTime", false)
+        }
+    }
+})
+
+Hooks.on("renderJournalPageSheet", (app, html, data) => {
+  const journal = app.document.parent;
+  if ( journal.getFlag(CONFIG.moduleId, CONFIG.journalFlag) ) html[0].classList.add(CONFIG.cssClass);
+});
